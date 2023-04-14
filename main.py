beaa40ecf6585c4aee9c0f6e0566ba3920b41f89
ChrisG19 <christopherg2808@gmail.com>
2023-04-14 14:34:45 -0600

    Funciones extra en main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 import tkinter as tk
 import json
+import datetime
+import os
 
 class HBaseSimulator:
     def __init__(self):
@@ -14,29 +16,44 @@ class HBaseSimulator:
 
         self.input_entry = tk.Entry(self.root, width=50)
         self.input_entry.pack()
+        self.input_entry.bind("<Return>", lambda event: self.execute_command())
 
         self.submit_button = tk.Button(self.root, text="Ejecutar", command=self.execute_command)
         self.submit_button.pack()
 
         self.output_text = tk.Text(self.root, height=20, width=80)
         self.output_text.pack()
+        
+        # Se crea un archivo de historial para tener los comandos ingresados
+        self.historial_file = "./data/historial.txt"
+        if not os.path.exists(self.historial_file):
+            with open(self.historial_file, "w") as f:
+                f.write("Comandos ingresados:\n")
 
     def execute_command(self):
         command = self.input_entry.get()
+        timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         
-        if command == 'clear':
+        # Se evalua si se limpiará o cerrara la "terminal"
+        if command == "clear":
             self.output_text.delete('1.0', tk.END)
+            
+        elif command.lower() == "exit":
+            self.root.quit()
 
         # Aquí podrías procesar el comando y obtener los resultados
         # En este ejemplo, simplemente lo mostraremos en la pantalla
         prueba = None
-        with open("./HFile.json") as archivo:
+        with open("./data/HFile.json") as archivo:
             # Se leen Regions con HFiles
             prueba = json.load(archivo)
 
             cs = command.split(" ")
             cm = cs[0].lower() # Comando Separado
-            print(cm)
+            
+            # Escribir el comando en el archivo de historial
+            with open(self.historial_file, "a") as f:
+                f.write(f"{timestamp}: {command}\n")
 
             if(cm == "create"):
                 pass
@@ -84,7 +101,12 @@ class HBaseSimulator:
                 self.output_text.insert(tk.END, "Es un put: " + cm + " \n")
 
             else:
-                self.output_text.insert(tk.END, "Comando desconocido" +"\n")
+                if cm == "clear":
+                    self.output_text.delete('1.0', tk.END)
+                else:
+                    self.output_text.insert(tk.END, "Comando desconocido" +"\n")
+        
+        self.input_entry.delete(0, tk.END)
         
 if __name__ == "__main__":
     hbase_simulator = HBaseSimulator()
