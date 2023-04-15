0abf21dfd6a566ed00573df24bb93d9162d4c452
ChrisG19 <christopherg2808@gmail.com>
2023-04-15 02:21:22 -0600

    Funciones DDL completadas V1

--- ddl/functions.py
+++ ddl/functions.py
@@ -1,4 +1,213 @@
+# Universidad del Valle de Guatemala
+# Bases de datos 2
+# HBase Simulation
+# Grupo#5
 
+import re
+from tkinter import simpledialog, messagebox
 
-def create(tableN, columnFamilies):
-    pass
+def get_info(command):
+    infoValues = command.split(" ", 1)[1]
+    dataInfo = infoValues.split(",", 1)
+    TableName = dataInfo[0].replace("'", "").replace('"', '')
+    return (TableName, dataInfo)
+
+def get_table(Hfiles, command):
+    Table = None
+    TableName = get_info(command)[0]
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            if table == TableName:
+                Table = Hfiles[region]
+                
+    return Table
+
+def get_table_names(Hfiles, regex):
+    table_names = []
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            if re.match(regex, table) != None:
+                table_names.append(table)
+                
+    return table_names
+
+def exists(Hfiles, command):
+    exist = False
+    if (get_table(Hfiles, command) != None):
+        exist = True
+    return exist
+
+def create(timestamp, command):
+    infoValues = command.split(" ", 1)[1]
+    dataInfo = infoValues.split(",", 1)
+    TableName = dataInfo[0].replace("'", "")
+    ColumnFamilies = dataInfo[1].strip().replace("'", "").replace('"', '').split(",")
+    
+    newRow = {}
+    for columnF in ColumnFamilies: newRow[columnF.strip()] = {}
+        
+    newHFile = { "enabled": "True", "timestamp": timestamp, "rows": {"0": newRow}}
+    new_Region = {TableName: newHFile}
+
+    return (TableName, new_Region)
+    
+def listTables(Hfiles):
+    table_names = []
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            table_names.append(table)
+                
+    return table_names
+
+def disableTable(Hfiles, command, timestamp):
+    TableName = get_info(command)[0]
+    if(not exists(Hfiles, command)):
+        return ("not found, no changes", TableName, Hfiles)
+    
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            if(TableName == table):
+                if "enabled" in Hfiles[region][table] and Hfiles[region][table]["enabled"] == "True":
+                    Hfiles[region][table]["timestamp"] = timestamp
+                    Hfiles[region][table]["enabled"] = "False"
+                    return ("disabled", TableName, Hfiles)
+                else:
+                    return ("Already disabled", TableName, Hfiles)
+
+def enableTable(Hfiles, command, timestamp):
+    TableName = get_info(command)[0]
+    if(not exists(Hfiles, command)):
+        return ("not found, no changes", TableName, Hfiles)
+    
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            if(TableName == table):
+                if "enabled" in Hfiles[region][table] and Hfiles[region][table]["enabled"] == "False":
+                    Hfiles[region][table]["timestamp"] = timestamp
+                    Hfiles[region][table]["enabled"] = "True"
+                    return ("enabled", TableName, Hfiles)
+                else:
+                    return ("Already enabled", TableName, Hfiles)
+
+def checkStatus(Hfiles, command):
+    if(not exists(Hfiles, command)):
+        return "Table not found, no changes"
+    TableName = get_info(command)[0]
+    isEnabled = False
+    
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            if(TableName == table):
+                if "enabled" in Hfiles[region][table]:
+                    if (Hfiles[region][table]["enabled"] == "True"):
+                        return not isEnabled
+                    else:
+                        return isEnabled
+                
+    return isEnabled
+
+def alterTable(Hfiles, command, timestamp):
+    TableName, CommandInfo = get_info(command)[0], [x.strip() for x in get_info(command)[1]][1]
+    if(not exists(Hfiles, command)):
+        return (Hfiles, "not found, no changes", None, TableName, None)
+    
+    ActionAndInfo = [x.strip() for x in CommandInfo.replace("'", "").replace('"', '').split("=>")]
+    
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            if TableName == table:
+                if "enabled" in Hfiles[region][table] and Hfiles[region][table]["enabled"] == "True":
+                    Hfiles[region][table]["timestamp"] = timestamp
+                    for row_key, row_value in list(Hfiles[region][table]["rows"].items()):
+                        for column_family, cfv in list(row_value.items()):
+                            if ActionAndInfo[0] == "delete":
+                                if column_family == ActionAndInfo[1]:
+                                    del Hfiles[region][table]["rows"][row_key][column_family]
+                            elif ActionAndInfo[0] == "ModifyName":
+                                tempInfo = [x.strip() for x in ActionAndInfo[1].split(",")]
+                                if column_family == tempInfo[0]:
+                                    Hfiles[region][table]["rows"][row_key][tempInfo[1]] = Hfiles[region][table]["rows"][row_key].pop(column_family)
+                                    return (Hfiles, ActionAndInfo[0], tempInfo[0], TableName, tempInfo[1])
+                else:
+                    return (Hfiles, "disabled", None, TableName, None)
+                                
+    return (Hfiles, ActionAndInfo[0], ActionAndInfo[1], TableName, None)
+    
+def dropTable(Hfiles, command):
+    TableName, CommandInfo = get_info(command)
+    
+    if(not exists(Hfiles, command)):
+        return f"Table {TableName} not found, no changes"
+    
+    veri = checkStatus(Hfiles, command)
+    
+    if(not veri == False):
+        return (f"{TableName} is not disabled, not changes", Hfiles)
+    
+    for region in Hfiles:
+        for table in Hfiles[region]:
+            if TableName == table:
+                del Hfiles[region]
+                return (f"{TableName} dropped", Hfiles)
+    
+def dropAll(Hfiles, command):
+    regex = get_info(command)[0]
+    coincidences = get_table_names(Hfiles, regex)
+    
+    if(len(coincidences) > 0):
+        messagebox.showinfo("Informacion", f"Encontramos {len(coincidences)} coincidencias {coincidences}")
+        res = simpledialog.askstring("Confirmación", f"Deseas dropear todas las tablas que coincidan con '{regex}'? (Y/n)")
+        if res and (res.lower() == "y"):
+            res2 = simpledialog.askstring("Confirmación", "Necesito tu autorización para deshabilitar antes las tablas.\nDeseas continuar? (Y/n)")
+            if res2 and (res2.lower() == "y"):
+                to_delete = []
+                for TableName in coincidences:
+                    for region in Hfiles:
+                        for table in Hfiles[region]:
+                            if(TableName == table):
+                                if "enabled" in Hfiles[region][table] and Hfiles[region][table]["enabled"] == "True":
+                                    Hfiles[region][table]["enabled"] = "False"
+                                to_delete.append((region, table))
+                messagebox.showinfo("Informacion", "Tablas deshabilitadas, procediendo a dropear")
+                for region, table in to_delete:
+                    del Hfiles[region]
+                return (f"{len(coincidences)} tables successfully dropped", Hfiles)
+            else:
+                print("Cancelando operacion")
+                return ("No changes", Hfiles)
+        else:
+            print("Cancelando operacion")
+            return ("No changes", Hfiles)
+    else:
+        return ("No hay coincidencias", Hfiles)
+    
+def describe(Hfiles, command):
+    TableName = get_info(command)[0]
+    if(not exists(Hfiles, command)):
+        return (f"Table {TableName} not found", TableName, Hfiles)
+    
+    HfileT = get_table(Hfiles, command)
+    status = ""
+    columnFnames = set()
+    countRows = 0
+    for table in HfileT:
+        for property in HfileT[table]:
+            if HfileT[table]["enabled"] == "True":
+                status = "ENABLED"
+            else:
+                status = "DISABLED"
+            for row in HfileT[table]["rows"]:
+                if row == '0':
+                    countRows = 0
+                else:
+                    countRows = len(HfileT[table]["rows"])
+                for value in HfileT[table]["rows"][row]:
+                    columnFnames.add(value)
+    columnFnames = list(columnFnames)
+    
+    column_info = ""
+    for column in columnFnames:
+        column_info += f"{{NAME => '{column}', DATA_BLOCK_ENCODING => 'NONE', BLOOMFILTER => 'ROW', REPLICATION_SCOPE => '0', VERSIONS => '1', COMPRESSION => 'NONE', MIN_VERSIONS => '0', TTL => 'FOREVER', KEEP_DELETED_CELLS => 'FALSE', BLOCKSIZE => '65536', IN_MEMORY => 'false', BLOCKCACHE => 'true'}}\n"
+
+    return f"Table {TableName} is {status}\n{TableName}\nCOLUMN FAMILIES DESCRIPTION\n{column_info}{countRows} row(s) in "
+    
--- main.py
+++ main.py
@@ -5,48 +5,66 @@ import tkinter as tk
 import json
 import datetime
 import os
+import time
+
+from ddl.functions import *
 
 class HBaseSimulator:
     def __init__(self):
+        
+        self.db = "./data/HFile.json"
+        
         self.root = tk.Tk()
         self.root.title("HBase Simulator")
 
         self.input_label = tk.Label(self.root, text="Ingrese comando:")
         self.input_label.pack()
 
-        self.input_entry = tk.Entry(self.root, width=50)
+        self.input_entry = tk.Entry(self.root, width=60)
         self.input_entry.pack()
         self.input_entry.bind("<Return>", lambda event: self.execute_command())
 
         self.submit_button = tk.Button(self.root, text="Ejecutar", command=self.execute_command)
         self.submit_button.pack()
 
-        self.output_text = tk.Text(self.root, height=20, width=80)
+        self.output_text = tk.Text(self.root, height=35, width=100)
         self.output_text.pack()
+        self.output_text.config(state=tk.DISABLED)
         
         # Se crea un archivo de historial para tener los comandos ingresados
         self.historial_file = "./data/historial.txt"
         if not os.path.exists(self.historial_file):
             with open(self.historial_file, "w") as f:
+                f.write("-----------------------\n")
                 f.write("Comandos ingresados:\n")
+        else:
+            with open(self.historial_file, "a") as f:
+                f.write("-----------------------\n")
+                f.write("Comandos ingresados:\n")
+                
+    def show_results(self, value):
+        self.output_text.config(state=tk.NORMAL)
+        self.output_text.insert(tk.END, value + "\n")
+        self.output_text.config(state=tk.DISABLED)
 
     def execute_command(self):
         command = self.input_entry.get()
-        timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        timestamp = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         
         # Se evalua si se limpiará o cerrara la "terminal"
         if command == "clear":
+            self.output_text.config(state=tk.NORMAL)
             self.output_text.delete('1.0', tk.END)
+            self.output_text.config(state=tk.DISABLED)
             
         elif command.lower() == "exit":
             self.root.quit()
 
-        # Aquí podrías procesar el comando y obtener los resultados
-        # En este ejemplo, simplemente lo mostraremos en la pantalla
-        prueba = None
-        with open("./data/HFile.json") as archivo:
+        # Procesamiento de comando
+        data = None
+        with open(self.db) as archivo:
             # Se leen Regions con HFiles
-            prueba = json.load(archivo)
+            data = json.load(archivo)
 
             cs = command.split(" ")
             cm = cs[0].lower() # Comando Separado
@@ -54,30 +72,128 @@ class HBaseSimulator:
             # Escribir el comando en el archivo de historial
             with open(self.historial_file, "a") as f:
                 f.write(f"{timestamp}: {command}\n")
+                
+            commands = [
+                "create", "list", "disable", "enable", "is_enabled", "alter",
+                "drop", "drop_all", "describe", "put", "get",
+                "scan", "delete", "delete_all", "count", "truncate" 
+            ]
+
+            if (cm != "clear"):
+                if cm in commands:
+                    tm = datetime.datetime.now().strftime("%S")
+                    cmdLine = f"\nhbase(main):{tm}> {command}"
+                    self.show_results(cmdLine)
+
+                else:
+                    tm = datetime.datetime.now().strftime("%S")
+                    cmdLine = f"\nhbase(main):{tm}> Comando {cm} desconocido"
+                    self.show_results(cmdLine)
 
             if(cm == "create"):
-                pass
+                timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+                n = len(data) + 1
+                nameRegion = "Region"+str(n)
+                start_time = time.time()
+                TableName, newHfile = create(timestamp, command)[0], create(timestamp, command)[1]
+
+                data[nameRegion] = newHfile
+                # Actualizar la db
+                with open(self.db, 'w') as f:
+                    json.dump(data, f, indent=4)
+
+                end_time = time.time()
+                tiempo = end_time - start_time
+
+                result = f"0 row(s) in {tiempo} seconds\n=> Hbase::Table - {TableName}"
+                self.show_results(result)
+                #print(json.dumps(newHfile, indent=4))
 
             elif(cm == "list"):
-                pass  
+                start_time = time.time()
+                table_names = listTables(data)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = "TABLE\n" + "\n".join(table_names) + f"\nTook {tiempo} seconds\n=> " + str(table_names)
+                self.show_results(result)
 
             elif(cm == "disable"):
-                pass  
+                timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+                start_time = time.time()  
+                resultDisable, TableName, newData = disableTable(data, command, timestamp)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = f"TABLE {TableName} {resultDisable} in {tiempo} seconds"
+                with open(self.db, 'w') as f:
+                    json.dump(newData, f, indent=4)
+                self.show_results(result)
+                
+            elif(cm == "enable"):
+                timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+                start_time = time.time()  
+                resultEnable, TableName, newData = enableTable(data, command, timestamp)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = f"TABLE {TableName} {resultEnable} in {tiempo} seconds"
+                with open(self.db, 'w') as f:
+                    json.dump(newData, f, indent=4)
+                self.show_results(result)
             
-            elif(cm == "isEnabled"):
-                pass  
+            elif(cm == "is_enabled"):
+                start_time = time.time()  
+                isEnabled = checkStatus(data, command)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = f"{isEnabled}\n0 row(s) in {tiempo} seconds"
+                self.show_results(result)
             
             elif(cm == "alter"):
-                pass  
+                timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+                start_time = time.time()  
+                alterData, action, cf, tname, newtname = alterTable(data, command, timestamp)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = "ERROR"
+                
+                if(action == 'disabled'):
+                    result = f"{tname} disabled, no changes\n0 row(s){tiempo} seconds"
+                elif (action == 'delete'):
+                    result = f"{cf} deleted from {tname} in {tiempo} seconds"
+                elif (action == 'ModifyName'):
+                    result = f"{cf} change to {newtname} in table {tname} in {tiempo} seconds"
+                else:
+                    result = f"Table {tname} {action} in {tiempo} seconds"
+                with open(self.db, 'w') as f:
+                    json.dump(alterData, f, indent=4)
+                self.show_results(result)
 
             elif (cm == "drop"):
-                self.output_text.insert(tk.END, "Es un put: " + cm + " \n")
+                start_time = time.time()  
+                dropResult, alterData = dropTable(data, command)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = f"{dropResult}\n0 row(s) in {tiempo} seconds"
+                with open(self.db, 'w') as f:
+                    json.dump(alterData, f, indent=4)
+                self.show_results(result)
 
             elif (cm == "drop_all"):
-                self.output_text.insert(tk.END, "Es un put: " + cm + " \n")
+                start_time = time.time()  
+                dropResult, alterData = dropAll(data, command)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = f"{dropResult}\n0 row(s) in {tiempo} seconds"
+                with open(self.db, 'w') as f:
+                    json.dump(alterData, f, indent=4)
+                self.show_results(result)
 
             elif (cm == "describe"):
-                self.output_text.insert(tk.END, "Es un put: " + cm + " \n")
+                start_time = time.time()  
+                result = describe(data, command)
+                end_time = time.time()
+                tiempo = end_time - start_time
+                result = f"{result}{tiempo} seconds"
+                self.show_results(result)
 
             elif (cm == "put"):
                 self.output_text.insert(tk.END, "Es un put: " + cm + " \n")
