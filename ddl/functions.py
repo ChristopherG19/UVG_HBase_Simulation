b7344170b01c54a82d984899f40d64f6be917ebf
ChrisG19 <christopherg2808@gmail.com>
2023-04-15 11:54:36 -0600

    Programación defensiva básica agregada

--- ddl/functions.py
+++ ddl/functions.py
@@ -7,7 +7,10 @@ import re
 from tkinter import simpledialog, messagebox
 
 def get_info(command):
-    infoValues = command.split(" ", 1)[1]
+    try:
+        infoValues = command.split(" ", 1)[1]
+    except:
+        return (None, "Sintaxis inválida: Argumentos faltantes")
     dataInfo = infoValues.split(",", 1)
     TableName = dataInfo[0].replace("'", "").replace('"', '')
     return (TableName, dataInfo)
@@ -15,6 +18,9 @@ def get_info(command):
 def get_table(Hfiles, command):
     Table = None
     TableName = get_info(command)[0]
+    if(TableName == None):
+        return Table
+    
     for region in Hfiles:
         for table in Hfiles[region]:
             if table == TableName:
@@ -38,10 +44,15 @@ def exists(Hfiles, command):
     return exist
 
 def create(timestamp, command):
-    infoValues = command.split(" ", 1)[1]
-    dataInfo = infoValues.split(",", 1)
-    TableName = dataInfo[0].replace("'", "")
-    ColumnFamilies = dataInfo[1].strip().replace("'", "").replace('"', '').split(",")
+    try:
+        infoValues = command.split(" ", 1)[1]
+        dataInfo = infoValues.split(",", 1)
+        TableName = dataInfo[0].replace("'", "")
+        ColumnFamilies = dataInfo[1].strip().replace("'", "").replace('"', '').split(",")
+        if((len(ColumnFamilies) == 1 and ColumnFamilies[0] == '') or any(x == " " for x in ColumnFamilies)):
+            return ("Sintaxis inválida: Argumentos faltantes", None)
+    except:
+        return ("Sintaxis inválida: Argumentos faltantes", None)
     
     newRow = {}
     for columnF in ColumnFamilies: newRow[columnF.strip()] = {}
@@ -61,6 +72,9 @@ def listTables(Hfiles):
 
 def disableTable(Hfiles, command, timestamp):
     TableName = get_info(command)[0]
+    if(TableName == None):
+        return (get_info(command)[1], None, Hfiles)
+        
     if(not exists(Hfiles, command)):
         return ("not found, no changes", TableName, Hfiles)
     
@@ -76,6 +90,9 @@ def disableTable(Hfiles, command, timestamp):
 
 def enableTable(Hfiles, command, timestamp):
     TableName = get_info(command)[0]
+    if(TableName == None):
+        return (get_info(command)[1], None, Hfiles)
+    
     if(not exists(Hfiles, command)):
         return ("not found, no changes", TableName, Hfiles)
     
@@ -92,7 +109,11 @@ def enableTable(Hfiles, command, timestamp):
 def checkStatus(Hfiles, command):
     if(not exists(Hfiles, command)):
         return "Table not found, no changes"
+    
     TableName = get_info(command)[0]
+    if(TableName == None):
+        return get_info(command)[1]
+    
     isEnabled = False
     
     for region in Hfiles:
@@ -108,10 +129,19 @@ def checkStatus(Hfiles, command):
 
 def alterTable(Hfiles, command, timestamp):
     TableName, CommandInfo = get_info(command)[0], [x.strip() for x in get_info(command)[1]][1]
+    
+    if(TableName == None):
+        return (Hfiles, get_info(command)[1], None, None, None)
+    
     if(not exists(Hfiles, command)):
         return (Hfiles, "not found, no changes", None, TableName, None)
     
-    ActionAndInfo = [x.strip() for x in CommandInfo.replace("'", "").replace('"', '').split("=>")]
+    try:
+        ActionAndInfo = [x.strip() for x in CommandInfo.replace("'", "").replace('"', '').split("=>")]
+        if (ActionAndInfo[0] == "" or ActionAndInfo[1] == ""):
+            return (Hfiles, "Sintaxis inválida: Argumentos faltantes", None, None, None)
+    except:
+        return (Hfiles, "Sintaxis inválida: Argumentos faltantes", None, None, None)
     
     for region in Hfiles:
         for table in Hfiles[region]:
@@ -136,22 +166,27 @@ def alterTable(Hfiles, command, timestamp):
 def dropTable(Hfiles, command):
     TableName, CommandInfo = get_info(command)
     
+    if(TableName == None):
+        return (CommandInfo, Hfiles, None)
+    
     if(not exists(Hfiles, command)):
-        return f"Table {TableName} not found, no changes"
+        return (f"Table {TableName} not found, no changes", Hfiles, TableName)
     
     veri = checkStatus(Hfiles, command)
     
     if(not veri == False):
-        return (f"{TableName} is not disabled, not changes", Hfiles)
+        return (f"{TableName} is not disabled, not changes", Hfiles, TableName)
     
     for region in Hfiles:
         for table in Hfiles[region]:
             if TableName == table:
                 del Hfiles[region]
-                return (f"{TableName} dropped", Hfiles)
+                return (f"{TableName} dropped", Hfiles, TableName)
     
 def dropAll(Hfiles, command):
     regex = get_info(command)[0]
+    if(regex == None):
+        return (get_info(command)[1], Hfiles, None)
     coincidences = get_table_names(Hfiles, regex)
     
     if(len(coincidences) > 0):
@@ -161,6 +196,7 @@ def dropAll(Hfiles, command):
             res2 = simpledialog.askstring("Confirmación", "Necesito tu autorización para deshabilitar antes las tablas.\nDeseas continuar? (Y/n)")
             if res2 and (res2.lower() == "y"):
                 to_delete = []
+                # Se deshabilitan las tablas
                 for TableName in coincidences:
                     for region in Hfiles:
                         for table in Hfiles[region]:
@@ -169,22 +205,27 @@ def dropAll(Hfiles, command):
                                     Hfiles[region][table]["enabled"] = "False"
                                 to_delete.append((region, table))
                 messagebox.showinfo("Informacion", "Tablas deshabilitadas, procediendo a dropear")
+                # Se dropean
                 for region, table in to_delete:
                     del Hfiles[region]
-                return (f"{len(coincidences)} tables successfully dropped", Hfiles)
+                return (f"{len(coincidences)} tables successfully dropped", Hfiles, "Ok")
             else:
                 print("Cancelando operacion")
-                return ("No changes", Hfiles)
+                return ("No changes", Hfiles, "Ok")
         else:
             print("Cancelando operacion")
-            return ("No changes", Hfiles)
+            return ("No changes", Hfiles, "Ok")
     else:
-        return ("No hay coincidencias", Hfiles)
+        return ("No hay coincidencias", Hfiles, "Ok")
     
 def describe(Hfiles, command):
     TableName = get_info(command)[0]
+    
+    if(TableName == None):
+        return (get_info(command)[1], 0, TableName)
+    
     if(not exists(Hfiles, command)):
-        return (f"Table {TableName} not found", TableName, Hfiles)
+        return (f"Table {TableName} not found", 0, TableName)
     
     HfileT = get_table(Hfiles, command)
     status = ""
@@ -209,5 +250,5 @@ def describe(Hfiles, command):
     for column in columnFnames:
         column_info += f"{{NAME => '{column}', DATA_BLOCK_ENCODING => 'NONE', BLOOMFILTER => 'ROW', REPLICATION_SCOPE => '0', VERSIONS => '1', COMPRESSION => 'NONE', MIN_VERSIONS => '0', TTL => 'FOREVER', KEEP_DELETED_CELLS => 'FALSE', BLOCKSIZE => '65536', IN_MEMORY => 'false', BLOCKCACHE => 'true'}}\n"
 
-    return f"Table {TableName} is {status}\n{TableName}\nCOLUMN FAMILIES DESCRIPTION\n{column_info}{countRows} row(s) in "
+    return (f"Table {TableName} is {status}\n{TableName}\nCOLUMN FAMILIES DESCRIPTION\n{column_info}", countRows, TableName)
     
